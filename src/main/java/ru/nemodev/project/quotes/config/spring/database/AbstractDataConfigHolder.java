a1114dccadc244114bef1a79840d636776288227
Nemodev <nemodeveloper1@gmail.com>
2019-07-07 19:27:48 +0300

    - переход на новый DB connection-pool

--- src/main/java/ru/nemodev/project/quotes/config/spring/database/AbstractDataConfigHolder.java
+++ src/main/java/ru/nemodev/project/quotes/config/spring/database/AbstractDataConfigHolder.java
@@ -1,9 +1,12 @@
 package ru.nemodev.project.quotes.config.spring.database;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import ru.nemodev.project.quotes.config.spring.property.DataBaseProperty;
+
+import java.util.concurrent.TimeUnit;
 
 
 public abstract class AbstractDataConfigHolder implements DataSourceHolder
@@ -12,18 +15,31 @@ public abstract class AbstractDataConfigHolder implements DataSourceHolder
     protected DataBaseProperty dataBaseProperty;
 
     @Bean(destroyMethod = "close")
-    public BasicDataSource dataSource()
+    public HikariDataSource dataSource()
     {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(dataBaseProperty.getDriver());
-        dataSource.setUrl(dataBaseProperty.getURL());
-        dataSource.setUsername(dataBaseProperty.getUsername());
-        dataSource.setPassword(dataBaseProperty.getPassword());
-        dataSource.setInitialSize(dataBaseProperty.getSocketTimeout());
-        dataSource.setMaxIdle(3);
-        dataSource.setPoolPreparedStatements(true);
-        dataSource.setRemoveAbandoned(true);
+        return new HikariDataSource(hikariConfig());
+    }
 
-        return dataSource;
+    @Bean
+    public HikariConfig hikariConfig()
+    {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(dataBaseProperty.getDriver());
+        config.setJdbcUrl(dataBaseProperty.getURL());
+        config.setUsername(dataBaseProperty.getUsername());
+        config.setPassword(dataBaseProperty.getPassword());
+        config.setConnectionTimeout(TimeUnit.SECONDS.toMillis(dataBaseProperty.getSocketTimeout()));
+        config.setLeakDetectionThreshold(TimeUnit.SECONDS.toMillis(10));
+        config.setMaxLifetime(TimeUnit.SECONDS.toMillis(180));
+        config.setIdleTimeout(TimeUnit.SECONDS.toMillis(120));
+        config.setAutoCommit(false);
+        config.setMinimumIdle(2);
+        config.setMaximumPoolSize(10);
+        config.setPoolName("HikariDataSourcePool");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        return config;
     }
 }
