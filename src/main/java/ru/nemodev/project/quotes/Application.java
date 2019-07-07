6efe0f9dd8b168af5a4f47d3d922813dd2cc42df
Nemodev <nemodeveloper1@gmail.com>
2019-07-08 00:19:26 +0300

    - доработка сбора статистики приложения

--- src/main/java/ru/nemodev/project/quotes/Application.java
+++ src/main/java/ru/nemodev/project/quotes/Application.java
@@ -1,6 +1,5 @@
 package ru.nemodev.project.quotes;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import ru.nemodev.project.quotes.config.spring.ApplicationConfig;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@EnableAdminServer
 @ComponentScan(basePackageClasses = {ApplicationConfig.class})
 public class Application
 {
